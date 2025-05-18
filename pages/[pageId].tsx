a9f4d9b6c684a3244a8ea9de6b4406c889d2f3b9
arcnworks <arcnworks@gmail.com>
2025-05-18 12:35:45 +0900

    deploy: 이미지 signed URL 최적화 및 정적 생성 안전화

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -14,17 +14,15 @@ import { getPageImageUrls } from 'notion-utils';
 import { mapImageUrl } from 'lib/map-image-url';
 
 /**
- * recordMap 내부 모든 이미지 URL을 mapImageUrl 로 치환해 줍니다.
+ * recordMap 내부 모든 이미지 URL을 mapImageUrl로 치환해 줍니다.
  * 이렇게 하면 빌드된 HTML에도 expired signed URL이 포함되지 않습니다.
  */
 function sanitizeRecordMap(recordMap: ExtendedRecordMap): ExtendedRecordMap {
-  // 모든 안전한 URL을 미리 수집
   getPageImageUrls(recordMap, { mapImageUrl }).forEach(() => {
-    // recordMap.block 을 순회하며 실제 값을 수정
     Object.entries(recordMap.block).forEach(([blockId, { value: block }]) => {
       if (!block) return;
 
-      // 이미지 블록인 경우 signed_urls 와 properties.source 치환
+      // 이미지 블록: signed_urls + properties.source 치환
       if (block.type === 'image') {
         if (recordMap.signed_urls?.[blockId]) {
           recordMap.signed_urls[blockId] = mapImageUrl(
@@ -50,7 +48,7 @@ function sanitizeRecordMap(recordMap: ExtendedRecordMap): ExtendedRecordMap {
         format.bookmark_icon = mapImageUrl(format.bookmark_icon, block)!;
       }
 
-      // 블록 아이콘 치환
+      // 블록 아이콘 치환 (타입스크립트 우회)
       if ((block as any).icon && typeof (block as any).icon === 'string') {
         (block as any).icon = mapImageUrl((block as any).icon, block)!;
       }
@@ -70,7 +68,9 @@ export const getStaticProps: GetStaticProps<PageProps, Params> = async (
     const props = await resolveNotionPage(domain, rawPageId);
 
     // 2) recordMap 내 모든 이미지 URL을 안전하게 치환
-    props.recordMap = sanitizeRecordMap(props.recordMap);
+    if ('recordMap' in props) {
+      props.recordMap = sanitizeRecordMap(props.recordMap);
+    }
 
     return {
       props,
@@ -120,7 +120,8 @@ function generateMeta(page: any) {
   return { title, description, image, url };
 }
 
-export default function NotionDomainDynamicPage(props: PageProps) {
+// Default export: props 타입을 any로 설정하여 page 프로퍼티 접근 오류 방지
+export default function NotionDomainDynamicPage(props: any) {
   const { page } = props;
   const meta = generateMeta(page);
 
